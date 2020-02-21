4a972e9e1466c13b647c36daac1e4da926f16eff
Rodrigo <professorbossini@gmail.com>
2020-02-21 17:09:16 -0300

    Operação de soma funcionando

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
- 
+ #include <stdio.h>
+ int main (){
+     char op;
+     int operando1, operando2;
+     printf ("Qual operação realizar?\n");
+     scanf ("%c", &op);
+     printf ("Qual o primeiro operando\n");
+     scanf ("%d", &operando1);
+     printf ("Qual o segundo operando?\n");
+     scanf ("%d", &operando2);
+     switch (op){
+         case '+':
+            printf ("%d + %d = %d\n", operando1, operando2, operando1 + operando2);
+            break;
+     }
+     return 0;
+ }
